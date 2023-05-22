0346f4d79efd2264e2a932fe0f45781fb1b601e6
Harishtn <tnharish2002@gmail.com>
2023-05-22 13:46:56 +0530

    Using Method

---
+++ Bike.java
@@ -0,0 +1,59 @@
+class Honda
+{
+
+//Fields- Global Variables
+// -static, -non-static
+
+static int dealer_id = 1234;  
+static String owner_name = "Kumar";
+int price;  //declaration ---->non-static
+int cc;
+float discount; //Data members
+
+public static void main(String [] args)
+
+{
+
+Honda shine   = new Honda(); //new allocates memory
+Honda activa  = new Honda();
+Honda unicorn = new Honda();
+
+shine.price=65000;
+activa.price=80000;
+unicorn.price=125000;
+
+shine.discount=5.03f;
+activa.discount=1.0f;
+unicorn.discount=2.0f;
+activa.buy(125000);  //Method calling statement
+shine.buy(1500000);
+
+Honda.offer();  //class calling
+offer();       //direct calling
+shine.offer(); //Object calling
+
+System.out.println(shine.price);
+
+System.out.println(Honda.owner_name);
+System.out.println(unicorn.owner_name);
+System.out.println(owner_name);
+
+
+}   
+
+void buy(int amount)
+{        //Method local variable
+
+System.out.println("Buying Honda Bike for Rs."+amount);
+//System.out.println(amount);
+
+}
+
+static void offer()
+{
+
+System.out.println("10% discount");
+
+}
+
+}
