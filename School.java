e91f8aaad9ad3e6e2c9bf2e7cf1cfaf264f06450
Harishtn <tnharish2002@gmail.com>
2023-05-24 18:32:23 +0530

    Fields Initialization Program

---
+++ School.java
@@ -0,0 +1,48 @@
+class School
+{
+
+int salary; // Feilds [Global variables] will be initialized with their default values.
+boolean b; 
+char c; 
+byte by; 
+short s; 
+long l; 
+float f; 
+double d; 
+String st;
+
+public static void main(String[] args)
+
+{
+
+
+School teacher=new School();
+teacher.teach();
+System.out.println("int "+teacher.salary);
+System.out.println("boolean "+ teacher.b);
+System.out.println("char "+teacher.c);
+System.out.println("byte "+teacher.by);
+System.out.println("short "+teacher.s);
+System.out.println("long "+teacher.l);
+System.out.println("float "+teacher.f);
+System.out.println("double "+teacher.d);
+System.out.println("String "+teacher.st);
+
+
+
+
+
+}
+
+void teach()
+{
+//int ab; 
+//System.out.println(ab);
+
+
+
+}
+
+
+
+}
