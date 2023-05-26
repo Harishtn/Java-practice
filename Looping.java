03551bc0fa7a2febf066aaff530d87bb07302813
Harishtn <tnharish2002@gmail.com>
2023-05-26 17:41:12 +0530

    Using If-else Condition

---
+++ Looping.java
@@ -0,0 +1,23 @@
+class Looping 
+{
+
+	public static void main(String[] args) 
+	{
+		int mi=200;
+		int gt=99;
+		
+		if(mi>gt)
+		{
+			System.out.println("MI enter the finals");
+			
+		}
+		else
+		{
+			System.out.println("GT enter the finals");
+		}
+		
+
+	}
+
+}
+
