0d7389e15ddcf41f0abdf1c18a5d48d46a158270
Harishtn <tnharish2002@gmail.com>
2023-05-27 22:11:58 +0530

    Conditional Statement using Boolean value

---
+++ IPL2.java
@@ -0,0 +1,22 @@
+class IPL2 
+{
+
+	
+	public static void main(String[] args)
+	{
+		int score = 220;//Assignment Operator 
+	    boolean rcb_trophy = false;
+	  // ==, === --> JavaScript
+	  if(rcb_trophy)
+	  {
+	    System.out.println("Get Married Soon");
+	  }
+	  else
+	  {
+	    System.out.println("Better Luck Next Year");
+	  }
+		
+		
+		
+	}
+}
