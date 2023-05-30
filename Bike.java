7b550cc4694f085f0bac301a56dd7d0549ea4e89
Harishtn <tnharish2002@gmail.com>
2023-05-30 15:54:48 +0530

    Using Method Program

--- Bike.java
+++ Bike.java
@@ -25,6 +25,7 @@ unicorn.price=125000;
 shine.discount=5.03f;
 activa.discount=1.0f;
 unicorn.discount=2.0f;
+
 activa.buy(125000);  //Method calling statement
 shine.buy(1500000);
 
@@ -44,7 +45,7 @@ System.out.println(owner_name);
 void buy(int amount)
 {        //Method local variable
 
-System.out.println("Buying Bike Bike for Rs."+amount);  //+ is concardination
+System.out.println("Buying Bike for Rs."+amount);  //+ is concardination
 //System.out.println(amount);
 
 }
