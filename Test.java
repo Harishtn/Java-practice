7be136290be74f1e0c4c04f29465402b2ad4976a
Harishtn <tnharish2002@gmail.com>
2023-05-31 13:36:29 +0530

    Using If Program

---
+++ Test.java
@@ -0,0 +1,18 @@
+package com.harish;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=10;
+		int j=2;
+		
+		if(i==10)
+		{
+			j=20;
+		}
+		System.out.println(j);
+	}
+
+}
