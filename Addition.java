cdf95f461c969c921723560e96577b3dbfca1445
Harishtn <tnharish2002@gmail.com>
2023-05-31 13:37:22 +0530

    Addition using If Program

---
+++ Addition.java
@@ -0,0 +1,22 @@
+package com.harish;
+
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		int total=0;
+		int number=1;
+		while(number<=5)
+		{
+			
+			total=total+number;
+			number=number+1;
+			
+		}
+		System.out.println(total);
+		
+		
+	}
+
+}
