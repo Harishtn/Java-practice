ee53ae15e3f2569da18d0e6812cf066523005b64
Harishtn <tnharish2002@gmail.com>
2023-05-31 13:39:35 +0530

    Multiplication using If Program

---
+++ Multiplication.java
@@ -0,0 +1,21 @@
+package com.harish;
+
+public class Multiplication 
+{
+
+	public static void main(String[] args)
+	{
+		int total=1;
+		int number=1;
+		while(number<=5)
+		{
+			
+			total=total*number;
+			number=number+1;
+			
+		}
+		System.out.println(total);
+
+	}
+
+}
