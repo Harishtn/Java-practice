6886d651d99299d78cabe60dbc2d6024a149dc52
Harishtn <tnharish2002@gmail.com>
2023-06-05 21:42:40 +0530

    Using Ladder If Program

---
+++ Pincode.java
@@ -0,0 +1,54 @@
+package com.harish;
+
+public class Pincode 
+{
+
+	public static void main(String[] args) 
+	{
+		int no=623009,i=1000,id=no/i;  
+	    ifElseCase(id);
+
+	}
+	private static void ifElseCase(int id) 
+	{
+	    if(id==600)
+	      System.out.println("Chennai");
+	    else if(id==604)
+	      System.out.println("Thiruvannamalai");
+	    else if(id==605)
+	      System.out.println("Cuddalore");
+	    else if(id==607)
+	      System.out.println("Trichi");
+	    else if(id==608)
+	      System.out.println("Ariyalur");
+	    else if(id==609)
+	      System.out.println("Nagapattinam");
+	    else if(id==613)
+	      System.out.println("Pudukkottai");
+	    else if(id==621)
+	      System.out.println("Sivagangai");
+	    else if(id==623)
+	      System.out.println("Ramanathapuram");
+	    else if(id==624)
+	      System.out.println("Dindigul");
+	    else if(id==625)
+	      System.out.println("Madurai");
+	    else if(id==626)
+	      System.out.println("Virudhunagar");
+	    else if(id==627)
+	      System.out.println("Tirunelveli");
+	    else if(id==628)
+	      System.out.println("Tuticorin");
+	    else if(id==642)
+	      System.out.println("Coimbatore");
+	    else if(id==635)
+	      System.out.println("Dharmapuri");
+	    else
+	      System.out.println("pincode is invalid");
+	    
+	 }
+
+	
+	
+	
+}
