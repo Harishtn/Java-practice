1073616c44423114a0a1a005b76704b9ff2f152d
Harishtn <tnharish2002@gmail.com>
2023-06-10 23:04:31 +0530

    Swapping of two numbers using two variables

--- Swapping2.java
+++ Swapping2.java
@@ -13,7 +13,7 @@ public class Swapping2
 	{
 		System.out.println("no1 "+no1);
 		System.out.println("no2 "+no2);   
-		no1=no1+no2; // Without the inclusion of third variable in swapping
+		no1=no1+no2;    // Without the inclusion of third variable in swapping
 		no2=no1-no2;
 		no1=no1-no2;
 		System.out.println("no1 "+no1);
