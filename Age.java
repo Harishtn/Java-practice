d3c4091398db4dd7a2000c7c9f5ad83aa0dc065e
Harishtn <tnharish2002@gmail.com>
2023-06-11 21:13:48 +0530

    Basic If-else Program

---
+++ Age.java
@@ -0,0 +1,17 @@
+package com.harish;
+
+public class Age
+{
+
+	public static void main(String[] args) 
+	{
+		int age=20;  
+	    //checking the age  
+	    if(age>18)
+	    {  
+	        System.out.print("Age is greater than 18");  
+	    }
+
+	}
+
+}
