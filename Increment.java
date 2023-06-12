cc652c30d3c27266a3910baf792f2d98345d6b66
Harishtn <tnharish2002@gmail.com>
2023-06-12 20:46:20 +0530

    Increment Operator using For-loop

---
+++ Increment.java
@@ -0,0 +1,17 @@
+package com.harish;
+
+public class Increment 
+{
+
+	public static void main(String[] args) 
+	{
+		  int no = 10; 
+		  System.out.println(no++);  //Post-Increment
+		  System.out.println(no);
+		  
+		  int n=5;
+		  System.out.println(++n);  //Pre-Increment
+		  System.out.println(n);
+	}
+
+}
