9356db4648780a8fc08c3982f78b9dda6d7b9ffb
Harishtn <tnharish2002@gmail.com>
2023-06-12 20:47:00 +0530

    Printing star using For-loop

---
+++ Star.java
@@ -0,0 +1,20 @@
+package com.harish;
+
+public class Star 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int star=1; star<=7; star++)
+		{
+		    System.out.print("* ");
+		}
+		System.out.println("Hi");
+		for(int star=1; star<=7; star++)
+		{
+		    System.out.print("* ");
+		}
+
+	}
+
+}
