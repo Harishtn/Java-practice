66ac6cd62b0ef74a901ee71ca039ac3d484e8916
Harishtn <tnharish2002@gmail.com>
2023-06-14 22:49:16 +0530

    Addition Program using For-loop

---
+++ Addition_for.java
@@ -0,0 +1,17 @@
+package com.harish;
+
+public class Addition_for 
+{
+
+	public static void main(String[] args) 
+	{
+		int total=0;
+		for(int number=1;number<=5;number++)
+		{
+			System.out.println(number);
+			total=total+number;
+		}
+		System.out.println(total);
+	}
+
+}
