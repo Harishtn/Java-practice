38297828373b75b167356109cdad2ebd48d06084
Harishtn <tnharish2002@gmail.com>
2023-06-18 22:31:16 +0530

    Using For-loop

---
+++ Looping_for.java
@@ -0,0 +1,15 @@
+package com.harish;
+
+public class Looping_for 
+{
+
+	public static void main(String[] args) 
+	{
+		  for(int month=5;month==5;)
+		  {
+		    System.out.println("Play");
+		  }
+
+	}
+
+}
