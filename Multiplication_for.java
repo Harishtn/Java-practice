ac65890502d7df6bb010fa0f38c8430af32f175b
Harishtn <tnharish2002@gmail.com>
2023-06-19 19:09:24 +0530

    Multiplication using For-loop

---
+++ Multiplication_for.java
@@ -0,0 +1,19 @@
+package com.harish;
+
+public class Multiplication_for 
+{
+
+	public static void main(String[] args) 
+	{
+		int total=1;
+		for(int number=1;number<=5;number++)
+		{
+			
+			total=total*number;
+			
+		}
+		System.out.println(total);
+
+	}
+
+}
