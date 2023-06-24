519a487dd2664b87304a46e6ac3f8f1e0af4ffff
Harishtn <tnharish2002@gmail.com>
2023-06-24 17:30:55 +0530

    Subtraction using For-loop

---
+++ Subtraction_for.java
@@ -0,0 +1,21 @@
+package com.harish;
+
+public class Subtraction_for
+{
+
+	public static void main(String[] args) 
+	{ 
+		int total=0;
+		for(int number=1;number<=5;number++)
+		{
+			
+			total=total-number;
+			
+		}
+		System.out.println(total);
+
+	}
+
+}
+
+
