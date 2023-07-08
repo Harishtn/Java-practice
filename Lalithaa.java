9f59faea66625347101e70630e5772ea1b1a7acc
Harishtn <tnharish2002@gmail.com>
2023-07-08 09:23:22 +0530

    Constructor Program

---
+++ Lalithaa.java
@@ -0,0 +1,25 @@
+package com.harish;
+
+public class Lalithaa 
+{
+	  int amt;  
+	  public Lalithaa(int amt)
+	  {
+		this();
+	    this.amt = amt; 
+	    System.out.println("Goddess Gift");
+	  }
+	  public Lalithaa() 
+	  {
+		System.out.println("Cool Drinks");
+	  }
+	  public static void main(String[] args) 
+	  {
+	    
+	    Lalithaa harish_ring = new Lalithaa(10000);
+	    Lalithaa bharani= new Lalithaa();
+	    
+	    
+	  }
+
+}
